33ee2bf7d74dac75b020ddab285d5b73d38f6481
RConant6 <rconant6@gmail.com>
2026-02-20 21:40:07 -0500

    [ASSETS] embedded default font

--- src/assets/assets.zig
+++ src/assets/assets.zig
@@ -6,3 +6,6 @@ pub const AssetManager = @import("AssetManager.zig");
 // Internal use only - used by renderer/text.zig
 pub const FilteredGlyph = @import("font/font_data.zig").FilteredGlyph;
 pub const glyph_builder = @import("font/glyph_builder.zig");
+
+// Embedded default font for tools that don't use the full AssetManager
+pub const embedded_default_font = @embedFile("default_orbitron.ttf");
