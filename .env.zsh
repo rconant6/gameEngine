a93cb318fd2b819d8331cfae2c37c6d09f9ba6e0
RConant6 <rconant6@gmail.com>
2026-03-07 21:27:44 -0500

    [BUILD] easy aliasing support

---
+++ .env.zsh
@@ -0,0 +1,12 @@
+# ~/Developer/zig/gameEngine/.env.zsh
+
+alias ui='zig build ui --prominent-compile-errors'
+alias test='zig build test --prominent-compile-errors'
+alias run='zig build play --prominent-compile-errors'
+alias release='zig build play --prominent-compile-errors -Doptimize=ReleaseFast'
+alias small='zig build play --prominent-compile-errors -Doptimize=ReleaseSmall'
+alias art='zig build art --prominent-compile-errors'
+alias clean='zig build clean'
+
+# Project dirs
+export ENGINE_ROOT=~/Developer/zig/gameEngine
