355746f6d159f9eb3055a5dad35be075e92e43d4
RConant6 <rconant6@gmail.com>
2026-03-09 18:33:07 -0400

    [BUILD] updated to call right artifact for art

--- .env.zsh
+++ .env.zsh
@@ -5,7 +5,7 @@ alias test='zig build test --prominent-compile-errors'
 alias run='zig build play --prominent-compile-errors'
 alias release='zig build play --prominent-compile-errors -Doptimize=ReleaseFast'
 alias small='zig build play --prominent-compile-errors -Doptimize=ReleaseSmall'
-alias art='zig build art --prominent-compile-errors'
+alias art='zig build zixelart --prominent-compile-errors'
 alias clean='zig build clean'
 
 # Project dirs
