12909113c1e10b7fb6e9448e1bd28be95944762d
RConant6 <rconant6@gmail.com>
2026-05-25 21:59:45 -0400

    [LINUX] added linux-dmabuf protoocol

--- src/platform/linux/wayland/c.zig
+++ src/platform/linux/wayland/c.zig
@@ -1,4 +1,5 @@
 pub const c = @cImport({
     @cInclude("wayland-client.h");
     @cInclude("xdg-shell-client-protocol.h");
+    @cInclude("linux-dmabuf-v1-client-protocol.h");
 });
--- src/platform/linux/wayland/consts.zig
+++ src/platform/linux/wayland/consts.zig
@@ -2,3 +2,4 @@ pub const wl_compositor = "wl_compositor";
 pub const wl_seat = "wl_seat";
 pub const wl_output = "wl_output";
 pub const xdg_wm_base = "xdg_wm_base";
+pub const zwp_linux_dmabuf_v1 = "zwp_linux_dmabuf_v1";
