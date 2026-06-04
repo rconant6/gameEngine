5deb690625d1bf01d81381dea3fd1c5cf4b3c8e4
RConant6 <rconant6@gmail.com>
2026-06-03 20:18:23 -0400

    [BUILD] env update
    changed to -error-style minimal_clear for readablity

--- .env.zsh
+++ .env.zsh
@@ -1,12 +1,12 @@
 # ~/Developer/zig/gameEngine/.env.zsh
-alias build='zig build --error-style minimal --summary all'
-alias ui='zig build ui --error-style minimal'
-alias test='zig build test --error-style minimal'
-alias run='zig build play --error-style minimal'
-alias release='zig build play --error-style minimal -Doptimize=ReleaseFast'
-alias small='zig build play --error-style minimal -Doptimize=ReleaseSmall'
-alias art='zig build zixelart --error-style minimal'
-alias level='zig build sceneEdit --error-style minimal'
+alias build='zig build --error-style minimal_clear --summary all'
+alias ui='zig build ui --error-style minimal_clear'
+alias test='zig build test --error-style minimal_clear'
+alias run='zig build play --error-style minimal_clear'
+alias release='zig build play --error-style minimal_clear -Doptimize=ReleaseFast'
+alias small='zig build play --error-style minimal_clear -Doptimize=ReleaseSmall'
+alias art='zig build zixelart --error-style minimal_clear'
+alias level='zig build sceneEdit --error-style minimal_clear'
 alias clean='zig build clean'
 
 # NOTE: --fuzz (continuous mode) is broken in Zig 0.16.0 (builtin.StackTrace type mismatch bug).
